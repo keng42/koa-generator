15517b443e9a466140fac44cfdac133900373098
keng42 <gh@keng42.com>
2017-08-04 18:49:30 +0800

    add mtkoa

---
+++ templates2/mtjs/config.js
@@ -0,0 +1,9 @@
+const mo = {};
+
+mo.server = {
+  port: 3000,
+  cookieKey1: 'ck1',
+  cookieKey2: 'cc2',
+};
+
+module.exports = mo;
---
+++ templates2/mtjs/routes/index.js
@@ -0,0 +1,7 @@
+const router = require('koa-router')();
+
+router.get('/', async (ctx) => {
+  ctx.body = 'hello world';
+});
+
+module.exports = router;
